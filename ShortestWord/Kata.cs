7f9b856f3c472a283f10be6bb4cc47a590363d47
Cosmin Butnaru <cosminbutnaru18@gmail.com>
2019-10-09 15:57:18 +0300

    Add project files.

---
+++ ShortestWord/Kata.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ShortestWord
+{
+    public class Kata
+    {
+        public static int FindShort(string s)
+        {
+            var results = new List<int>();
+            var words = s.Split(' ');
+
+            foreach (var word in words)
+            {
+                results.Add(word.Length);
+            }
+
+            return results.Min();
+        }
+    }
+}
---
+++ UnitTestShortestWord/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestShortestWord")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestShortestWord")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9b41b25b-6f28-4a1b-ae33-9e1adfd5b5c4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
